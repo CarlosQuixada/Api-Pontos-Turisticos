b3ae56f41b5d7c7f5e45d5455147e2db2da720a0
CarlosQuixada <carlos@trustvox.com.br>
2019-10-21 15:33:44 -0300

    Create Core

---
+++ core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class PontoTuristico(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    aprovado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
